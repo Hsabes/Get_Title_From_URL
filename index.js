57fccc9b98fff3d180fc0e6d602626bf6ff3f796
Harrison Sabean <92425655+Hsabes@users.noreply.github.com>
2023-02-21 11:01:47 -0500

    Created repo and pasted code from original files

---
+++ index.js
@@ -0,0 +1,21 @@
+function getTitle(string){
+    let text = document.querySelector('.text');
+    let generate = document.querySelector('.get-text');
+    let display = document.querySelector("h2")
+    let copy = document.querySelector('.copy')
+    let isCopied = document.querySelector('p')
+    generate.addEventListener('click', () => {
+      let val = text.value.split('/').slice(-1)[0];
+      val = val.replace(/[-]/g, ' ').split(' ').map((word) => word.charAt(0).toUpperCase() + word.substring(1)).join(' ');
+      display.textContent = text.value.match(/[/]/g).length > 4 ? '_' + val : val;
+      isCopied.textContent = ''
+    })
+    copy.addEventListener('click', () => {
+      let textToCopy = display.textContent;
+      navigator.clipboard.writeText(textToCopy);
+      text.value = ''
+      isCopied.textContent = 'Copied!'
+    })
+}
+
+getTitle();
